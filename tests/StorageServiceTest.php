1b87f2907bbde3e1186844133a7b0b142a05797c
Adam Gajzlerowicz <nelf86@gmail.com>
2016-01-12 21:16:11 +0000

    Storage Service tests finished

--- tests/StorageServiceTest.php
+++ tests/StorageServiceTest.php
@@ -1,30 +1,29 @@
 <?php
 
-
+Use Services\StorageService;
 
 class StorageServiceTest extends PHPUnit_Framework_TestCase{
-    protected $service;
-    public function __construct($name, array $data, $dataName)
-    {
-        $this->service = new \Services\StorageService();
-        $this->service->setStorageName('storage/storageTest.json');
-    }
 
     public function testLoadData(){
-        $data = $this->service->getData();
+        $service = new StorageService();
+        $service->setStorageName('storage/storageTest.json');
+        $data = $service->getData();
+        $this->assertEquals(1, 1);
         $this->assertInternalType('array',$data);
     }
-    public function loadDataJuggleAndSave(){
-        $data = $this->service->getData();
+    public function testLoadAndSave(){
+        $service = new Services\StorageService();
+        $service->setStorageName('storage/storageTest.json');
+        $data = $service->getData();
         $dataSize = count($data);
         $randomNumber = rand(0,$dataSize);
         $randomName = uniqid();
         $randomSecondName = uniqid();
-        $data[$randomNumber]['firstname'] = $randomName;
-        $data[$randomNumber]['surname'] = $randomSecondName;
-        $this->service->saveData($data);
-
-        $restoredData = $this->service->getData();
+        $data[$randomNumber]->firstname = $randomName;
+        $data[$randomNumber]->surname = $randomSecondName;
+        $service->saveData($data);
+        $restoredData = $service->getData();
+        $this->assertInternalType('array',$restoredData);
         $this->assertEquals($restoredData, $data);
     }
 }
